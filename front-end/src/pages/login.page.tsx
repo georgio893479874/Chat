afe4531d664a5815757a4f0e4b4847e6cfb82447
georgio893479874 <129616431+georgio893479874@users.noreply.github.com>
2024-03-16 21:40:00 +0200

    Update login.page.tsx

--- front-end/src/pages/login.page.tsx
+++ front-end/src/pages/login.page.tsx
@@ -1,7 +1,7 @@
 import { Button, Paper, Stack, TextField } from "@mui/material"
 import { createClient } from '@supabase/supabase-js'
 import { useState } from "react";
-// pkVt66vqc7dZZpEk
+
 let key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0Y2Rkb3FybmZteWZpZW5wcGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4MzM2MDIsImV4cCI6MjAyNTQwOTYwMn0.o4rnb00r_igMaBWXuV1V1FmWekva49nKLrwhndN3y2c';
 let url = 'https://stcddoqrnfmyfienppic.supabase.co';
 const supabase = createClient(url, key);
